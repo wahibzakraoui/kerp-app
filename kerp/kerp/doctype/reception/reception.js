a0cfe0f5c2605f07ecda3c77e42e43f21770ddeb
Wahib ZAKRAOUI <wahibza22@gmail.com>
2022-10-18 20:08:56 +0100

    Added first doctype

---
+++ kerp/kerp/doctype/reception/reception.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Wahib ZAKRAOUI and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Reception', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ kerp/kerp/doctype/reception/test_reception.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Wahib ZAKRAOUI and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestReception(FrappeTestCase):
+	pass
